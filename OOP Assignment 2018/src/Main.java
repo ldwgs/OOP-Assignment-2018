a4492cf0548c143e5a53e3fdd79098fc2a6474af
ludwig <lute_p@hotmail.com>
2018-07-04 16:41:57 +0800

    .

---
+++ OOP Assignment 2018/src/Main.java
@@ -0,0 +1,11 @@
+import java.sql.*;
+
+public class Main {
+	public static void main(String[] args) {
+		try { // Create connection to database
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
